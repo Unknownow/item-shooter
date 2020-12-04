8cd404ca1fcfeaaa42fe348d569db91c49a68133
Cường Bùi <cuong.bpt82@gmail.com>
2020-12-04 12:05:03 +0700

    sửa một chút event system

--- Assets/Scripts/Utilities/EventManager/EventSystem.cs
+++ Assets/Scripts/Utilities/EventManager/EventSystem.cs
@@ -8,6 +8,10 @@ public class EventSystem
         return this.GetType().Name;
     }
 
+    // ========== Fields and properties ==========
+    /// <summary>
+    /// Singleton instance
+    /// </summary>
     private static EventSystem _instance;
 
     public static EventSystem instance
@@ -20,12 +24,31 @@ public class EventSystem
         }
     }
 
+    /// <summary>
+    /// Event listeners dictionary
+    /// Store all listeners 
+    /// </summary>
     private Dictionary<string, List<EventListener>> _eventList = null;
-    public EventSystem()
+
+    private long _eventListenerCount;
+
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    private EventSystem()
     {
         _eventList = new Dictionary<string, List<EventListener>>();
+        _eventListenerCount = 0;
     }
 
+    // ========== Public methods ==========
+    /// <summary>
+    /// Add listener to certain event code
+    /// </summary>
+    /// <param name="eventCode">Event code</param>
+    /// <param name="caller">Object create this listener</param>
+    /// <param name="callback">Callback to call when event is dispatched</param>
+    /// <returns></returns>
     public EventListener AddListener(string eventCode, Object caller, Action<object[]> callback)
     {
         LogUtils.instance.Log(GetClassName(), "AddListener", "EVENT_CODE:", eventCode);
@@ -35,12 +58,18 @@ public class EventSystem
         EventListener listener = GetListener(eventCode, caller, callback);
         if (listener == null)
         {
-            listener = new EventListener(eventCode, eventCode + "_" + listEventListener.Count.ToString(), caller, callback);
+            listener = new EventListener(eventCode, eventCode + "_" + _eventListenerCount, caller, callback);
             listEventListener.Add(listener);
+            _eventListenerCount += 1;
         }
         return listener;
     }
 
+    /// <summary>
+    /// Dispatch certain event.
+    /// </summary>
+    /// <param name="eventCode">Event code</param>
+    /// <param name="eventParam">Param of event</param>
     public void DispatchEvent(string eventCode, object[] eventParam = null)
     {
         LogUtils.instance.Log(GetClassName(), "DispatchEvent", "EVENT_CODE:", eventCode);
@@ -50,6 +79,7 @@ public class EventSystem
             LogUtils.instance.Log(GetClassName(), "DispatchEvent", "EVENT_CODE:", eventCode, "DOES NOT HAVE ANY SUBCRIBERS");
             return;
         }
+
         foreach (EventListener listener in listEventListener)
         {
             if (listener.isEnabled && !listener.isRemoved)
@@ -63,12 +93,22 @@ public class EventSystem
         }
     }
 
+    /// <summary>
+    /// Set enable for listener.
+    /// </summary>
+    /// <param name="listener"></param>
+    /// <param name="isEnabled"></param>
     public void SetEnabledListener(EventListener listener, Boolean isEnabled)
     {
         LogUtils.instance.Log(GetClassName(), "DispatchEvent", "LISTENER_ID::", listener.listenerId, "IS_ENABLED", isEnabled.ToString());
         listener.isEnabled = isEnabled;
     }
 
+    /// <summary>
+    /// Remove listener from list.
+    /// </summary>
+    /// <param name="eventCode">Event code of listener</param>
+    /// <param name="listener">Listener want to remove</param>
     public void RemoveListener(string eventCode, EventListener listener)
     {
         LogUtils.instance.Log(GetClassName(), "RemoveListener", "EVENT_CODE:", eventCode, "LISTENER_ID:", listener.listenerId);
@@ -81,6 +121,10 @@ public class EventSystem
         existedListener.isRemoved = true;
     }
 
+    /// <summary>
+    /// Remove all listeners of certain event code.
+    /// </summary>
+    /// <param name="eventCode"></param>
     public void RemoveAllListenersOfEvent(string eventCode)
     {
         LogUtils.instance.Log(GetClassName(), "RemoveAllListenersOfEvent", "EVENT_CODE:", eventCode);
@@ -89,11 +133,20 @@ public class EventSystem
             listener.isRemoved = true;
     }
 
+    /// <summary>
+    /// Remove all listeners
+    /// </summary>
     public void RemoveAllListeners()
     {
         _eventList.Clear();
     }
 
+    // ========== Private methods ==========
+    /// <summary>
+    /// Get listener list of certain event code
+    /// </summary>
+    /// <param name="eventCode"></param>
+    /// <returns>List of listeners</returns>
     private List<EventListener> GetListenersList(string eventCode)
     {
         if (!_eventList.ContainsKey(eventCode))
@@ -106,6 +159,12 @@ public class EventSystem
         return listEventListener;
     }
 
+    /// <summary>
+    /// Get listener by ID
+    /// </summary>
+    /// <param name="eventCode"></param>
+    /// <param name="listenerId"></param>
+    /// <returns></returns>
     private EventListener GetListener(string eventCode, string listenerId)
     {
         List<EventListener> listEventListener = GetListenersList(eventCode);
@@ -114,6 +173,13 @@ public class EventSystem
         return listEventListener.Find(listener => listener.listenerId.Equals(listenerId));
     }
 
+    /// <summary>
+    /// Get listener by caller and callback
+    /// </summary>
+    /// <param name="eventCode"></param>
+    /// <param name="caller"></param>
+    /// <param name="callback"></param>
+    /// <returns></returns>
     private EventListener GetListener(string eventCode, Object caller, Action<object[]> callback)
     {
         List<EventListener> listEventListener = GetListenersList(eventCode);
@@ -123,6 +189,9 @@ public class EventSystem
     }
 }
 
+/// <summary>
+/// Event listener object
+/// </summary>
 public class EventListener
 {
     public string listenerId;
